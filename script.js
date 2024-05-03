b5812981e99a3056aa5ae4dd24da8bb17ad27e2e
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-03 11:53:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function toCase(text) {
-  // write your code here
-	if (text.length === 0) {
-		return '-';
-	}else{
-		// let res='';
-		for (let i = 0; i < text.length; i++) {
-			let res+= text[i].toLowerCase() +'-' +text[i].toUpperCase()
-		return res;
-	
-		}
-	
+    // Check if the input text is empty
+    if (text.length === 0) {
+        return '-';
+    } else {
+        let result = '';
+        for (let i = 0; i < text.length; i++) {
+            // Convert each character to lowercase and append '-' followed by its uppercase version
+            result += text[i].toLowerCase() + '-' + text[i].toUpperCase();
+        }
+        return result;
+    }
 }
+
+
+
 const text = prompt("Enter text:");
 alert(toCase(text));
